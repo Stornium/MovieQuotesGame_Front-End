fcca01edaedd2958e00c532593cee47baed951c8
Christophe LALATAHIANA <christophe.lalatahiana@etu.univ-nantes.fr>
2018-02-23 15:44:37 +0100

    titre de la page change en fonction du ng-view

--- js/script.js
+++ js/script.js
@@ -2,6 +2,38 @@ var app = angular.module("myApp", ["ngRoute"]);
 app.config(function($routeProvider) {
     $routeProvider
     .when("/", {
-        templateUrl : "vue/accueil.html"
-    });
+        templateUrl : "vue/accueil.html",
+        controller : accueil
+    })
+    .when("/connexion", {
+        templateUrl : "vue/connexion.html",
+        controller : connexion
+    })
+      .otherwise({
+  		redirectTo: '/'
+  	});
 });
+
+// app.controller('myCtrl', function($scope) {
+//     $scope.title = "TITLE";
+// });
+
+app.factory('Page', function(){
+  var title = 'default';
+  return {
+    title: function() { return title; },
+    setTitle: function(newTitle) { title = newTitle; }
+  };
+});
+
+app.controller('myCtrl', function($scope, Page) {
+  $scope.Page = Page;
+});
+
+function accueil($scope, Page) {
+  Page.setTitle('accueil');
+}
+
+function connexion($scope, Page) {
+  Page.setTitle('connexion');
+}
