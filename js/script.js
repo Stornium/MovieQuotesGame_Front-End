9e680ba28a0c5056d17dc7444def3c72b75f685c
Christophe LALATAHIANA <christophe.lalatahiana@etu.univ-nantes.fr>
2018-02-26 11:20:55 +0100

    ajout squelette creation + lien

--- js/script.js
+++ js/script.js
@@ -2,24 +2,21 @@ var app = angular.module("myApp", ["ngRoute"]);
 app.config(function($routeProvider) {
     $routeProvider
     .when("/", {
-        templateUrl : "vue/accueil.html",
-        controller : accueil
-    })
-    .when("/connexion", {
         templateUrl : "vue/connexion.html",
         controller : connexion
+    })
+    .when("/creation", {
+        templateUrl : "vue/creation.html",
+        controller : creation
     })
       .otherwise({
   		redirectTo: '/'
   	});
 });
 
-// app.controller('myCtrl', function($scope) {
-//     $scope.title = "TITLE";
-// });
 
 app.factory('Page', function(){
-  var title = 'default';
+  var title = 'acceuil';
   return {
     title: function() { return title; },
     setTitle: function(newTitle) { title = newTitle; }
@@ -30,10 +27,10 @@ app.controller('myCtrl', function($scope, Page) {
   $scope.Page = Page;
 });
 
-function accueil($scope, Page) {
-  Page.setTitle('accueil');
-}
-
 function connexion($scope, Page) {
   Page.setTitle('connexion');
+}
+
+function creation($scope, Page) {
+  Page.setTitle('creation de compte');
 }
