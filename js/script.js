f4a869d0ce03266aa68cd917f855bf0b72d62886
Christophe LALATAHIANA <christophe.lalatahiana@etu.univ-nantes.fr>
2018-02-28 12:01:53 +0100

    ajout squelette des pages restantes

--- js/script.js
+++ js/script.js
@@ -1,23 +1,35 @@
 var app = angular.module("myApp", ["ngRoute"]);
-app.config(function($routeProvider) {
-    $routeProvider
-    .when("/", {
-        templateUrl : "vue/connexion.html",
-        controller : connexion
-    })
-    .when("/creation", {
-        templateUrl : "vue/creation.html",
-        controller : creation
-    })
-    .when("/vote", {
-        templateUrl : "vue/vote.html",
-        controller : vote
-    })
-    .otherwise({
-  		redirectTo: '/'
-  	});
-});
 
+app.config(function($routeProvider) {
+  $routeProvider
+  .when("/", {
+    templateUrl : "vue/connexion.html",
+    controller : connexion
+  })
+  .when("/creation", {
+    templateUrl : "vue/creation.html",
+    controller : creation
+  })
+  .when("/vote", {
+    templateUrl : "vue/vote.html",
+    controller : vote
+  })
+  .when("/classement", {
+    templateUrl : "vue/classement-utilisateurs.html",
+    controller : classement
+  })
+  .when("/citations-recentes", {
+    templateUrl : "vue/citations-recentes.html",
+    controller : citationsRecentes
+  })
+  .when("/profil", {
+    templateUrl : "vue/profil.html",
+    controller : profil
+  })
+  .otherwise({
+    redirectTo: '/'
+  });
+});
 
 app.factory('Page', function(){
   var title = 'acceuil';
@@ -31,6 +43,15 @@ app.controller('myCtrl', function($scope, Page) {
   $scope.Page = Page;
 });
 
+
+app.controller('Categories', function($scope, $http) {
+    $http.get('https://api.chucknorris.io/jokes/categories').
+        then(function(response) {
+            $scope.categ = response.data;
+        });
+});
+
+
 function connexion($scope, Page) {
   Page.setTitle('connexion');
 }
@@ -40,12 +61,18 @@ function creation($scope, Page) {
 }
 
 function vote($scope, Page) {
-  Page.setTitle('vote')
+  Page.setTitle('vote');
 }
 
-app.controller('Categories', function($scope, $http) {
-    $http.get('https://api.chucknorris.io/jokes/categories').
-        then(function(response) {
-            $scope.categ = response.data;
-        });
-});
+function classement($scope, Page) {
+  Page.setTitle('classement');
+}
+
+function citationsRecentes($scope, Page) {
+  Page.setTitle('citations récentes');
+}
+
+function profil($scope, Page) {
+  Page.setTitle('profil');
+}
+
